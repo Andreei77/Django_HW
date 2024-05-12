011583d95f925fa4a9e740c9c17627c10efe3028
Andreei77 <hodzera@gmail.com>
2024-05-12 17:58:18 +0300

    one

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 
 def home(request):
-    return HttpResponse('<h1>Главная</h1>')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Наш клуб</h1>')
+    return render(request, 'blog/about.html', {'title': 'О клубе Python Bytes'})
