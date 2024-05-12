42483d4c9fcd82287e285592ee8f92042908e145
Andreei77 <hodzera@gmail.com>
2024-05-12 20:54:43 +0300

    reg

--- users/views.py
+++ users/views.py
@@ -10,8 +10,13 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Создан аккаунт {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Ваш аккаунт создан: можно войти на сайт.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
